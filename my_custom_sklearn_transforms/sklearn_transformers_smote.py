27013c311233bbeb7100aafad95ce2d1316d783f
Fabio Dias <mfabiodias@gmail.com>
2020-08-19 01:00:06 -0300

    Separando as classes

---
+++ my_custom_sklearn_transforms/sklearn_transformers_smote.py
@@ -0,0 +1,14 @@
+from imblearn.over_sampling import SMOTE, ADASYN
+
+class Smote(BaseEstimator, TransformerMixin):
+    def __init__(self):
+           self.smote = SMOTE(SMOTE(kind='regular', n_jobs=-1))
+
+    def fit(self, X, y=None):
+           return self
+
+    def transform(self, X, y):
+        data = X.copy()
+        targets = y.copy()
+        data, targets = self.smote.fit_resample(data , targets)
+        return data, targets
