daa3ccbe95909b194a6e48f85d5c0e7f2dc44768
Fabio Dias <mfabiodias@gmail.com>
2020-08-19 10:43:49 -0300

    Ajuste classe Smote 6

--- my_custom_sklearn_transforms/sklearn_transformers_smote.py
+++ my_custom_sklearn_transforms/sklearn_transformers_smote.py
@@ -1,30 +1,30 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from imblearn.over_sampling import SMOTE, ADASYN
 
-# class Smote(BaseEstimator, TransformerMixin):
-#     def __init__(self):
-#         self.smote = SMOTE()
-
-#     def fit(self, X, y):
-#         return self
-
-#     def transform(self, X, y):
-#         data = X.copy()
-#         targets = y.copy()
-#         data, targets = self.smote.fit_resample(data, targets)
-#         return data, targets
-
 class Smote(BaseEstimator, TransformerMixin):
-    def fit(self, X, y):
-        print(X.shape, ' ', type(X)) # (57, 28)   <class 'numpy.ndarray'>
-        print(len(y), ' ', type)     #    57      <class 'list'>
-        self.smote = SMOTE(kind='regular', n_jobs=-1).fit(X, y)
+    def __init__(self):
+        self.smote = SMOTE()
 
+    def fit(self, X, y):
         return self
 
-    def fit_transform(self, X, y):
-        self.fit(X, y)
-        return self.smote.fit_resample(X, y)
+    def transform(self, X, y):
+        # data = X.copy()
+        # targets = y.copy()
+        data, targets = self.smote.fit_resample(X, y)
+        return data, targets
 
-    def transform(self, X):
-        return X
+# class Smote(BaseEstimator, TransformerMixin):
+#     def fit(self, X, y):
+#         print(X.shape, ' ', type(X)) # (57, 28)   <class 'numpy.ndarray'>
+#         print(len(y), ' ', type)     #    57      <class 'list'>
+#         self.smote = SMOTE(kind='regular', n_jobs=-1).fit(X, y)
+
+#         return self
+
+#     def fit_transform(self, X, y):
+#         self.fit(X, y)
+#         return self.smote.fit_resample(X, y)
+
+#     def transform(self, X):
+#         return X
